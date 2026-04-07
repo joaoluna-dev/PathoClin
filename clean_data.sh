01b6845d69f872b2c6c1413f68141926d4a8a737
joaoluna-dev <joaogabrieldeluna@gmail.com>
2026-04-06 21:25:27 -0300

    Atualização do nome do projeto

--- clean_data.sh
+++ clean_data.sh
@@ -8,7 +8,7 @@ NC='\033[0m' #serve para voltar o terminal para a cor normal
 
 if [[ "$target" == "temp_files" ]]; then
   echo -e "${CYAN}"
-  echo "Iniciando limpeza dos dados temporários..."
+  echo "PathoClin - Iniciando limpeza dos dados temporários..."
   echo -e "${NC}"
   rm data/temp/*.intervar
   rm data/temp/*.vcf
@@ -16,7 +16,7 @@ if [[ "$target" == "temp_files" ]]; then
   rm data/temp/*.json
 elif [[ "$target" == "vcfs" ]]; then
   echo -e "${CYAN}"
-  echo "Iniciando limpeza dos vcfs..."
+  echo "PathoClin - Iniciando limpeza dos vcfs..."
   echo -e "${NC}"
   rm data/raw/*
 fi
